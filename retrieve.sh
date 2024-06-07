13e84a56b9a25dead7f575d4af7b40adf2899eb7
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2024-06-07 14:05:49 +0200

    ask again if not found

--- retrieve.sh
+++ retrieve.sh
@@ -3,25 +3,28 @@
 source colors.sh
 
 retrieve() {
-    echo "${BLUE}Enter the website name to retrieve the profile: ${NC}"
-    read website
+    while true; do
+        echo "${BLUE}Enter the website name to retrieve the profile: ${NC}"
+        read website
 
-    # Read the file and search for the corresponding website
-    profile=$(grep "^$website," passwords.txt)
-    
-    if [ -z "$profile" ]; then
-        echo "${RED}No profile found for the given website.${NC}"
-    else
-        # Split the profile line into fields
-        IFS=',' read -r found_website username encrypted_password <<< "$profile"
+        # Read the file and search for the corresponding website
+        profile=$(grep "^$website," passwords.txt)
         
-        # Decrypt the password
-        password=$(echo "$encrypted_password" | openssl enc -d -des3 -base64 -pass pass:mypasswd -pbkdf2)
-        
-        echo "${GREEN}Website: $found_website${NC}"
-        echo "${GREEN}Username: $username${NC}"
-        echo "${GREEN}Password: $password${NC}"
-    fi
+        if [ -z "$profile" ]; then
+            echo "${RED}No profile found for the given website. Please try again.${NC}"
+        else
+            # Split the profile line into fields
+            IFS=',' read -r found_website username encrypted_password <<< "$profile"
+            
+            # Decrypt the password
+            password=$(echo "$encrypted_password" | openssl enc -d -des3 -base64 -pass pass:mypasswd -pbkdf2)
+            
+            echo "${GREEN}Website: $found_website${NC}"
+            echo "${GREEN}Username: $username${NC}"
+            echo "${GREEN}Password: $password${NC}"
+            break
+        fi
+    done
 }
 
 retrieve_all() {
