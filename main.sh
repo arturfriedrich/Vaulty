29a51cda9a74e5d412c5621a1e8a5fc071b6d5dc
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2024-06-07 13:58:49 +0200

    new menu

--- main.sh
+++ main.sh
@@ -5,13 +5,17 @@ source retrieve.sh
 source delete.sh
 
 echo "Welcome to Vaulty! What would you like to do?"
-echo "(a)dd profile | (f)ind profile data | (r)etrieve all profile data | (d)elete profile data | (q)uit"
-read choice
 
-case $choice in
-    "a") create ;;
-    "f") retrieve ;;
-    "r") retrieve_all ;;
-    "d") delete ;;
-    *) echo "Invalid choice." ;;
-esac
+while true; do
+    echo "(a)dd profile | (f)ind profile data | (r)etrieve all profile data | (d)elete profile data | (q)uit"
+    read choice
+
+    case $choice in
+        "a") create ;;
+        "f") retrieve ;;
+        "r") retrieve_all ;;
+        "d") delete ;;
+        "q") echo "Goodbye!" ; exit 0 ;;
+        *) echo "Invalid choice." ;;
+    esac
+done
